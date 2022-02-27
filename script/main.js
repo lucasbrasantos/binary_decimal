d7d04b283240d755bc3ea12aa42fbef4c7b02270
lucasbrasantos <lucasbrasantos@gmail.com>
2022-02-26 22:23:33 -0300

    first commit

---
+++ script/main.js
@@ -0,0 +1,20 @@
+const binary = ;
+
+var num = binary.toString().split("").reverse().join("");
+var decimal = 0;
+
+for (let i = 0; i < num.length; i++){
+    let result = convert(num[i], i)
+    if (result != null) {
+    	decimal += result;
+    }
+}
+
+function convert(value, index){
+    if(value == 1){
+        return 2**index;
+    }
+    return null;
+}
+
+console.log(`binario: ${binary}\ndecimal: ${decimal}`);
